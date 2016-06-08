fa6365aa7441024166cd29d5b8009c2b2fdc955b
meh2481 <meh2481@email.vccs.edu>
2016-06-07 22:40:57 -0400

    Fleshing out Animation class

--- Animation.cpp
+++ Animation.cpp
@@ -1,9 +1,11 @@
 #include "Animation.h"
+#include <QFontMetrics>
 
 Animation::Animation()
 {
     sizeX = sizeY = maxWidth = 0;
     curRender = NULL;
+    bgTrans = sheetTrans = false;
 }
 
 int Animation::width()
@@ -19,95 +21,235 @@ int Animation::height()
 void Animation::setMaxWidth(int width)
 {
     maxWidth = width;
+    redraw();
 }
 
 QImage* Animation::getImage()
 {
-    return curRender;   //TODO test if null or whatever
+    return curRender;
 }
 
 void Animation::addFrame(QImage* img, int pos)
 {
-    frames.insert(pos, img);
+    if(pos < 0)
+        frames.append(img);
+    else
+        frames.insert(pos, img);
+    redraw();
 }
 
 QImage* Animation::getFrame(int pos)
 {
-    return NULL; //TODO
+    if(pos < frames.size() || pos > 0)
+        return NULL;
+    return frames.at(pos);
 }
 
 void Animation::deleteFrame(int pos)
 {
-
+    if(pos < frames.size() && pos > 0)
+    {
+        frames.removeAt(pos);
+        redraw();
+    }
 }
 
 int Animation::getFrame(int x, int y)
 {
-    return 0;   //TODO
+    int iFrame = 0;
+    foreach(QRect rc, frameRects)
+    {
+        if(rc.contains(x, y))
+            return iFrame;
+        iFrame++;
+    }
+
+    return -1;
 }
 
 void Animation::setTitle(QString str)
 {
-
+    title = str;
+    redraw();
 }
 
 void Animation::setFont(QFont f)
 {
-
+    font = f;
+    redraw();
 }
 
 void Animation::setBgTransparent(bool bTrans)
 {
-
+    bgTrans = bTrans;
+    redraw();
 }
 
 void Animation::setBgFill(QColor col)
 {
-
+    bgFill = col;
+    redraw();
 }
 
 void Animation::setSheetFill(QColor col)
 {
-
+    sheetFill = col;
+    redraw();
 }
 
 void Animation::setSheetTransparent(bool bTrans)
 {
-
+    sheetTrans = bTrans;
+    redraw();
 }
 
 void Animation::setSelectedCol(QColor col)
 {
-
+    selectedCol = col;
+    redraw();
 }
 
 void Animation::setFrameSelectCol(QColor col)
 {
-
+    frameSelectedCol = col;
+    redraw();
 }
 
 void Animation::redraw()
 {
+    if(curRender && (curRender->width() != sizeX || curRender->height() != sizeY))
+    {
+        delete curRender;
+        curRender = NULL;
+    }
+    if(!curRender)
+        curRender = new QImage(sizeX, sizeY, QImage::Format_ARGB32);
 
+    //TODO
 }
 
 void Animation::reverse()
 {
+    QList<QImage*> revFrames = frames;
+    frames.clear();
+    foreach(QImage* img, revFrames)
+        frames.prepend(img);
 
+    recalculate();
+    redraw();
 }
 
-void Animation::removeDuplicates()
+bool Animation::removeDuplicates()
 {
+    bool bFoundDuplicates = false;
 
+    for(int tester = 0; tester < frames.size(); tester++)
+    {
+        for(int testee = tester+1; testee < frames.size(); testee++)
+        {
+            if(frames.at(testee)->width() != frames.at(tester)->width() || frames.at(testee)->height() != frames.at(tester)->height()) continue;
+            if(frames.at(testee)->byteCount() != frames.at(tester)->byteCount()) continue;
+
+            if(std::strncmp((const char*)frames.at(testee)->bits(), (const char*)frames.at(tester)->bits(), frames.at(testee)->byteCount()) == 0)
+            {
+                bFoundDuplicates = true;
+                frames.removeAt(testee);
+                testee--;
+            }
+        }
+    }
+
+    if(bFoundDuplicates)
+    {
+        recalculate();
+        redraw();
+    }
+
+    return bFoundDuplicates;
 }
 
+void Animation::recalculate()
+{
+    QFontMetrics fm(font);
+    float textHeight = fm.height() + FONT_SPACING;
 
+    frameRects.clear();
 
+    //Figure out dimensions of final image
+    sizeY = spaceY;
+    if(title.length() && titleEnabled)
+        sizeY += textHeight;
+    sizeX = 0;
+    int curSizeX = spaceX;
+    int ySize = 0;
+    foreach(QImage* img, frames)
+    {
+        //Test to see if we should start next line
+        if(curSizeX + img->width() + spaceX > maxWidth)
+        {
+            sizeY += spaceY + ySize;
+            ySize = 0;
+            if(curSizeX > sizeX)
+                sizeX = curSizeX;
+            curSizeX = spaceX;
+        }
 
+        //Add rectangle for this image
+        QRect r(sizeX, sizeY, img->width(), img->height());
+        frameRects.append(r);
 
+        if(img->height() > ySize)
+            ySize = img->height();
 
+        curSizeX += img->width() + spaceX;
+    }
 
+    if(curSizeX > sizeX)
+        sizeX = curSizeX;
 
+    sizeY += spaceY + ySize;
+}
+
+void Animation::setSpacing(int x, int y)
+{
+    bool changed = false;
+    if(x != spaceX || y != spaceY)
+        changed = true;
+
+    spaceX = x;
+    spaceY = y;
+
+    if(changed)
+    {
+        recalculate();
+        redraw();
+    }
+}
+
+void Animation::setBgImage(QImage bg)
+{
+    bgImage = bg;
+}
+
+void Animation::draggingTo(int x, int y)
+{
+    //TODO
+}
+
+void Animation::droppedAt(int x, int y, QList<QImage*> images)
+{
+    //TODO
+}
+
+void Animation::balance(/*TODO*/)
+{
+    //TODO
+}
+
+void Animation::saveAs(QString GIFFilename)
+{
+    //TODO
+}
 
 
 
--- Animation.h
+++ Animation.h
@@ -5,16 +5,31 @@
 #include <QColor>
 #include <QString>
 #include <QFont>
+#include <QRect>
+
+#define FONT_SPACING 3
 
 class Animation
 {
 protected:
     QList<QImage*> frames;
+    QList<QRect> frameRects;
     int maxWidth;
     int sizeX, sizeY;
+    int spaceX, spaceY;
     QImage* curRender;
     QString title;
+    bool titleEnabled;
     QFont font;
+    bool bgTrans;
+    bool sheetTrans;
+    QColor bgFill;
+    QColor sheetFill;
+    QColor selectedCol;
+    QColor frameSelectedCol;
+    QImage bgImage; //Image to draw if bg is transparent
+
+    void recalculate();
 
 public:
     Animation();
@@ -26,22 +41,62 @@ public:
     void setTitle(QString str);
     void setFont(QFont f);
     void setBgTransparent(bool bTrans);
+    void setBgImage(QImage bg);
     void setBgFill(QColor col);
     void setSheetFill(QColor col);
     void setSheetTransparent(bool bTrans);
     void setSelectedCol(QColor col);
     void setFrameSelectCol(QColor col);
+    void setSpacing(int x, int y);
+    void setNameEnabled(bool enabled);
 
     void redraw();
     void reverse();
-    void removeDuplicates();
+    bool removeDuplicates();    //Return true if there were duplicates, false otherwise
     QImage* getImage();
 
-    void addFrame(QImage* img, int pos = 0);
+    void addFrame(QImage* img, int pos = -1);
     QImage* getFrame(int pos);
     void deleteFrame(int pos);
+    void mouseOverFrame(int pos);
+    void selectFrame(int pos);
+
+    void draggingTo(int x, int y);  //Dragging to here; draw some sort of visual cue
+    void droppedAt(int x, int y, QList<QImage*> images);    //Dropped these images here; add them
+
+    void balance(/*TODO*/);
+    void saveAs(QString GIFFilename);
 
     int getFrame(int x, int y);
 };
 
 #endif // ANIMATION_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
